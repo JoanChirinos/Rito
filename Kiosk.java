30ecab6b4f30f1c053bd07926ad4200a1175414d
jwongUlti25 <jwong25@stuy.edu>
2018-01-08 13:00:50 -0500

    start Kiosk.java

---
+++ Kiosk.java
@@ -0,0 +1,37 @@
+/*
+    ____     _    __         
+   / __ \   (_)  / /_   ____ 
+  / /_/ /  / /  / __/  / __ \
+ / _, _/  / /  / /_   / /_/ /
+/_/ |_|  /_/   \__/   \____/ 
+                             
+------------------------------------
+Aaron Li, Johnny Wong, Joan Chirinos
+------------------------------------
+*/
+
+
+/**********************************************************************
+ <notes and comments>
+ **********************************************************************/
+
+public class Kiosk {
+    // inst vars
+    CreditCard cc;
+    String user;
+
+    // constructor
+    public Kiosk(){
+	// assign cc to user inputted ccNum via Keyboard.java?
+	// assign user to user's inputted name
+    }
+
+    public static String checkExp() {
+	// check cc's E
+    }
+    
+    public static void main (String [] args) {
+	start();
+    }
+    
+}
