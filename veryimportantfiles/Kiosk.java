45514a73a51146ca4c433d2708133a08b172fe9e
jwongUlti25 <jwong25@stuy.edu>
2018-01-10 12:51:40 -0500

    added signatures for Kiosk.java methods

--- veryimportantfiles/Kiosk.java
+++ veryimportantfiles/Kiosk.java
@@ -26,12 +26,49 @@ public class Kiosk {
 	// assign user to user's inputted name
     }
 
-    public static String checkExp() {
-	// check cc's E
+    public static String strArray(String [] x) {
+	// stringify inputted String array
     }
     
-    public static void main (String [] args) {
-	start();
+    public static String checkExp() {
+	// check cc's expiration date
     }
+
+    //*******************movie methods***************************
+
+    public static void viewCollection() {
+	// view top 100 movies 
+    }
+
+    public static void search() {
+	// user inputs String via Keyboard.java
+	// String contains info pertaining to movie(s) titles
+    }
+    
+
+    //***********************************************************
+
+    
+    //*******************checkout methods************************
+
+    public static void listPrevRentals() {
+	// check previous rentals
+	// and print them in a vertical list
+    }
+
+    public static void listOrders() {
+	// print current orders for movies
+	// in a vertical list
+    }
+
+    public static void receipt() {
+	// print out a receipt for the user
+    }
+
+    //***********************************************************
+    public static void start(){
+	// start Kiosk
+    }
+    
     
 }
