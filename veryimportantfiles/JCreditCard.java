e39601f7d841781d089ba783d2f11f85a077ff43
JoanChirinos <jchirinos@stuy.edu>
2018-01-10 21:06:00 -0500

    Added flowchart (flow.pdf)

--- veryimportantfiles/JCreditCard.java
+++ veryimportantfiles/JCreditCard.java
@@ -20,34 +20,44 @@ public class JCreditCard {
 
     public JCreditCard() {
 
-	//set creditcard number
-	for (int i = 0; i < 9; i++)
-	    _ccnum += (int)(Math.random()*10);
+	//set creditcard number. If that number exists, try again
+	while (true) {
+	    for (int i = 0; i < 9; i++)
+		_ccnum += (int)(Math.random()*10);
+	    System.out.println(creditCardExists(_ccnum));
+	    if (!(creditCardExists(_ccnum))) break;
+	}
+	//set 4-digit pin. Save it encrypted
 	_pin = "";
 	while (true) {
 	    System.out.print("4 digit pin: ");
 	    _pin = Keyboard.readString();
-	    try {
-		int numpin = Integer.parseInt(_pin);
-		if (numpin >= 0 && numpin <= 9999)
-		    break;
-		else System.out.println("Invalid pin\n");
-	    }
-	    catch (Exception e) {
+	    if (_pin.length() != 4)
 		System.out.println("Invalid pin\n");
+	    else {
+		try {
+		    int numpin = Integer.parseInt(_pin);
+		    if (numpin >= 0 && numpin <= 9999)
+			break;
+		    else System.out.println("Invalid pin\n");
+	    }
+		catch (Exception e) {
+		    System.out.println("Invalid pin\n");
+		}
 	    }
 	}
-	System.out.println(_pin);
-	pinEncrypt();
-	System.out.println(_pin);
-
+	_pin = pinEncrypt(_pin);
     }//end CreditCard()
 
     //checks csv file to see if credit card number is valid
-    public boolean creditCardExists() {
-	return true;
-    }
-
+    public boolean creditCardExists(String ccnum) {
+	CSVRW c = new CSVRW("creditcards.csv");
+	for (int i = 0; i < c.get().size(); i++) {
+	    if (c.get(i, 0).equals(ccnum)) return true;
+	}
+	return false;
+    }//end creditCardExists
+    
     /*
     public JCreditCard(String cc, String pin) {
 
@@ -57,14 +67,14 @@ public class JCreditCard {
     */
 
     //changes pin to encrypted pin
-    private void pinEncrypt() {
-        String strpin = "" + ((Integer.parseInt(_pin) + 1111) * 1121231234);
-	System.out.println(strpin);
-	_pin = "";
+    private String pinEncrypt(String pin) {
+        String strpin = "" + ((Integer.parseInt(pin) + 1111) * 1121231234);
+	String retpin = "";
 	for (int i = 0; i < strpin.length(); i++) {
 	    int index = Integer.parseInt(strpin.substring(i, i + 1));
-	    _pin += ALPHABET.substring(index, index + 1);
+	    retpin += ALPHABET.substring(index, index + 1);
 	}
+	return retpin;
     }//end pinEncrypt
 
     public String getccnum() {
