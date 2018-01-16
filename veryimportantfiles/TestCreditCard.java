f4099d1ce11cf51243683881a2006f10807ca5b1
aaronli39 <aaronli39@gmail.com>
2018-01-16 18:57:03 -0500

    fixed driver

--- veryimportantfiles/TestCreditCard.java
+++ veryimportantfiles/TestCreditCard.java
@@ -5,12 +5,12 @@ public class TestCreditCard {
      public static void main(String[] args) {
           CreditCard test = new CreditCard();
 
-          long output = test.cardPin;
+
 
           // String output = "" + (int) (Math.random() * 10);
           // output += (int) (Math.random() * 10);
 
-          System.out.println(output);
+          System.out.println(test.cardPin);
 
 
      }//end main
